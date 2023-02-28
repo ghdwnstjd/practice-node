da342fa0b49694d6886ac4c9daa6f2e0af5aace3
ghdwnstjd <dltmdgheorkf@naver.com>
2023-02-28 17:16:15 +0900

    두 번째 수업 (2/28)

---
+++ os/os.js
@@ -0,0 +1,5 @@
+const os = require('os');
+
+console.log(os.cpus());
+
+// os 사용 갯수
---
+++ require_module-exports/var.js
@@ -0,0 +1,15 @@
+const odd='홀수입니다.';
+const even='짝수입니다.';
+
+// module.exports = {
+//     odd,
+//     even,
+// };
+
+module.exports = {odd, even};
+
+// module.exports = [odd, even]; //배열로도 가능
+
+// module.exports는 아래와 같이 생략이 가능하다.
+// exports.odd = odd;
+// exports.even = even;
